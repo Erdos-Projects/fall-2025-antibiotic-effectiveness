0e11808c70ee8b80c0bb109e250d1a33681190a0
Haejun Oh <ohhu@mail.uc.edu>
2025-10-28 09:42:53 -0500

    added results pdf and directory and editted the final log reg model with calibration

--- Modeling/best_models/logistic_regression_optimized.py
+++ Modeling/best_models/logistic_regression_optimized.py
@@ -1,123 +1,245 @@
 # Import libraries
-import pandas as pd
-import numpy as np
 import os
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 from re import X
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.calibration import CalibrationDisplay
-from sklearn.metrics import (make_scorer, confusion_matrix, f1_score, precision_score, recall_score, PrecisionRecallDisplay)
+from sklearn.calibration import calibration_curve, CalibratedClassifierCV
+from sklearn.metrics import (make_scorer, confusion_matrix, 
+                             f1_score, precision_score, 
+                             brier_score_loss, precision_recall_curve, 
+                             accuracy_score, recall_score, get_scorer,
+                             auc, roc_curve)
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline
 from scipy.stats import uniform
 from sklearn.model_selection import RandomizedSearchCV
 
-"""Model setup"""
 
-# build pipeline
-pipe = Pipeline([
-    ('scaler', StandardScaler()),
-    ('smote', SMOTE(random_state=312)),
-    ('logreg', LogisticRegression(max_iter=2000))
-])
+# Class: Model Trainer
+class ModelTrainer:
+    def __init__(self, param_grid, scoring, pipe, kf, random_state=312):
+        self.param_grid = param_grid
+        self.scoring = scoring
+        self.pipe = pipe
+        self.kf = kf
+        self.random_state = random_state
+    
+    def train(self, X_train, y_train):
+        # randomized serach for best hyper-parameters
+        search = RandomizedSearchCV(
+            estimator=self.pipe,
+            param_distributions=self.param_grid,
+            cv=self.kf,
+            scoring=self.scoring,
+            n_jobs=-1,
+            n_iter=100,
+            random_state=self.random_state,
+            refit='f1_weighted'
+        )
+        search.fit(X_train, y_train)
+        
+        return search
 
-random_state = 312
-# 5 kfold cv for hyperparameter otimization
-kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=random_state)
+    def calibrate(self, best_model, X_train, y_train):
+        # calibrate the model
+        X_res, y_res = best_model.named_steps['smote'].fit_resample(X_train, y_train)
+        calibrated_model = CalibratedClassifierCV(best_model.named_steps['logreg'], method='sigmoid', cv='prefit')
+        calibrated_model.fit(X_res, y_res)
 
-# model parameters
-param_grid = {
-    'logreg__C': uniform(0.1, 10),
-    'logreg__solver': ['liblinear', 'lbfgs']
-}
+        return calibrated_model
 
-# Custom scorer for False Negative Rate
-def fnr(y_test, y_pred):
-    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-    fnr = fn / (fn + tp)
-    return fnr
+class ModelEvaluator:
+    def __init__(self, scoring, output_dir):
+        self.scoring = scoring
+        self.output_dir = output_dir
+        os.makedirs(self.output_dir, exist_ok=True)
 
-# Scoring dictionary
-scoring = {
-    'accuracy': 'accuracy',
-    'precision': make_scorer(precision_score, pos_label=1),
-    'recall': make_scorer(recall_score, pos_label=1),
-    'f1': make_scorer(f1_score, pos_label=1),
-    'roc_auc': 'roc_auc',
-    'fnr': make_scorer(fnr),
-    'f1_macro': make_scorer(f1_score, average='macro', pos_label=1),
-    'f1_weighted': make_scorer(f1_score, average='weighted', pos_label=1)
-}
+    def evaluate_cv(self, search, antibiotic):
+        # evaluate CV training dataset metrics
+        cv_results = search.cv_results_
+        scores = {"Antibiotic" : antibiotic}
+        print("\nAverage CV Metrics for Best Model:")
+        for metric in self.scoring.keys():
+            mean_score = cv_results[f'mean_test_{metric}'][search.best_index_]
+            std_score = cv_results[f'std_test_{metric}'][search.best_index_]
+            scores[metric] = (mean_score, std_score)
+            print(f"{metric}: {mean_score:.3f} ± {std_score:.3f}")
+        return scores
+    
+    def evaluate_test(self, best_model, calibrated_model, X_test, y_test, antibiotic):
+        # evaluate testing dataset metrics
+        y_pred_pre= best_model.predict(X_test)
+        y_proba_pre = best_model.predict_proba(X_test)[:, 1]
+        y_pred_post= calibrated_model.predict(X_test)
+        y_proba_post = calibrated_model.predict_proba(X_test)[:, 1]
 
-"""Run model with hyperparameter tuning"""
-dir = 'Data/final_dataframes/'
-antibiotics = ['Gentamicin', 'Trimethoprim_Sulfamethoxazole', 'Ciprofloxacin',
-                'Ampicillin', 'Cefazolin','Nitrofurantoin','Piperacillin_Tazobactam',
-                'Levofloxacin', 'Ceftriaxone']
-metric_results = []
+        test_result = {"Antibiotic" : antibiotic}
+        test_result["brier_pre"] = brier_score_loss(y_test, y_proba_pre)
+        test_result["brier_post"] = brier_score_loss(y_test, y_proba_post)
 
-for i in antibiotics:
-    print('\n')
-    print(f"==== Antibiotic: {i} ====")
-    train_data = pd.read_csv(f'{dir}{i}_train_data.csv')
-    X_train = train_data.drop(i, axis=1)
-    y_train_raw = train_data[i]
-    # one hot incoding such that resistance cases are positive cases
-    y_train = (y_train_raw==2.0).astype(int)
+        for metric_name, scorer_key in self.scoring.items():
+            scorer = get_scorer(scorer_key)
+            test_result[f"{metric_name}_pre"] = scorer(best_model, X_test, y_test)
+            test_result[f"{metric_name}_post"] = scorer(calibrated_model, X_test, y_test)
+            print(f"{metric_name}_pre: {test_result[f'{metric_name}_pre']:.3f}")
+            print(f"{metric_name}_post: {test_result[f'{metric_name}_post']:.3f}")
 
-    grid = RandomizedSearchCV(
-        estimator=pipe,
-        param_distributions=param_grid,
-        cv=kf,
-        scoring=scoring,
-        n_jobs=-1,
-        n_iter=100,
-        random_state=random_state,
-        refit='f1_weighted' # f1 weighted is the metric used to refit the model
-    )
+        print(f"brier_pre: {test_result['brier_pre']:.3f}")
+        print(f"brier_post: {test_result['brier_post']:.3f}")
+        print("\n")
+        return test_result
 
-    # Fit grid search
-    grid.fit(X_train, y_train)
+class PlotGenerator:
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
+        os.makedirs(self.output_dir, exist_ok=True)
+    
+    def plot_all(self, antibiotic, y_test, y_pred_post, y_proba_post, y_proba_pre):
+        fig, ax = plt.subplots(2, 2, figsize=(12, 10))
+        fig.suptitle(f"Logistic Regression Model Evaluation for {antibiotic}", fontsize=16, fontweight='bold')
 
-    # Print best parameters per each antibiotic
-    print("Best parameters:", grid.best_params_)
-    print("Best score:", grid.best_score_)
+        # ROC curve
+        fpr, tpr, _ = roc_curve(y_test, y_proba_post)
+        roc_auc = auc(fpr, tpr)
+        ax[0,0].plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+        ax[0,0].plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        ax[0,0].set_title("ROC curve")
+        ax[0,0].set_xlabel("False Positive Rate")
+        ax[0,0].set_ylabel("True Positive Rate")
+        ax[0,0].legend()
 
-    # Get cross-validation results for all metrics
-    results = grid.cv_results_
+        # Precision Recall curve
+        precision, recall, _ = precision_recall_curve(y_test, y_proba_post)
+        ax[0,1].plot(recall, precision, color='purple', lw=2)
+        ax[0,1].set_title("Precision-Recall Curve")
+        ax[0,1].set_xlabel("Recall")
+        ax[0,1].set_ylabel("Precision")
 
-    print("\nAverage CV Metrics for Best Model:")
+        # Calibration plot
+        prob_true_pre, prob_pred_pre = calibration_curve(y_test, y_proba_pre, n_bins=10,strategy='quantile')
+        prob_true_post, prob_pred_post = calibration_curve(y_test, y_proba_post, n_bins=10,strategy='quantile')
+        ax[1,0].plot(prob_pred_pre, prob_true_pre, marker='x', linestyle='-', label="Before Calibration")
+        ax[1,0].plot(prob_pred_post, prob_true_post, marker='o', linestyle='-', label="After Calibration")
+        ax[1,0].plot([0,1],[0,1], color='navy', linestyle='--', label="Perfect Calibration")
+        ax[1,0].set_title("Calibration Curve")
+        ax[1,0].set_xlabel("Predicted Probability")
+        ax[1,0].set_ylabel("True Probability")
+        ax[1,0].legend()
 
-    # Save the metrics in a csv file
-    scores = {"Antibiotic" : i}
-    for metric in scoring.keys():
-        mean_score = results[f'mean_test_{metric}'][grid.best_index_]
-        std_score = results[f'std_test_{metric}'][grid.best_index_]
-        scores[metric] = (mean_score, std_score)
-        print(f"{metric}: {mean_score:.3f} ± {std_score:.3f}")
+        # Confusion matrix
+        cm = confusion_matrix(y_test, y_pred_post)
+        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", ax=ax[1, 1])
+        ax[1, 1].set_title("Confusion Matrix")
+        ax[1, 1].set_xlabel("Predicted")
+        ax[1, 1].set_ylabel("Actual")
 
-    metric_results.append(scores)
+        plt.tight_layout(rect=[0, 0, 1, 0.96])
+        plt.savefig(os.path.join(self.output_dir, f"logreg_modeleval_{antibiotic}.png"), dpi=300)
+        plt.close()
 
-    # Get Predicted Probabilities on Validation Data
-    best_model = grid.best_estimator_
+    def plot_feature_importance(self, best_model, X_train, antibiotic):
+        logreg = best_model.named_steps['logreg']
+        feature_names = X_train.columns
+        coefs = logreg.coef_.flatten()
+        feature_importance = pd.DataFrame({
+            'feature': feature_names,
+            'importance': coefs
+        }).sort_values(by='importance', key=abs, ascending=False)
 
-    # Create and save PR curve and calibration plots
-    fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+        # Save as CSV
+        feature_importance.to_csv(os.path.join(self.output_dir, f"feature_importance_{antibiotic}.csv"), index=False)
 
-    PrecisionRecallDisplay.from_estimator(best_model, X_train, y_train, ax=ax[0])
-    ax[0].set_title("Precision-Recall Curve")
+        # Plot
+        plt.figure(figsize=(8, 6))
+        sns.barplot(data=feature_importance.head(15), x='importance', y='feature', palette='coolwarm')
+        plt.title(f"Top 15 Feature Importances - {antibiotic}")
+        plt.tight_layout()
+        plt.savefig(os.path.join(self.output_dir, f"feature_importance_{antibiotic}.png"))
+        plt.close()
 
-    CalibrationDisplay.from_estimator(best_model, X_train, y_train, ax=ax[1])
-    ax[1].set_title("Calibration Plot")
 
-    plt.tight_layout()
-    plotdir = 'Modeling/best_models/training_metric_results/'
-    plt.savefig(os.path.join(plotdir,f'{i}_calibration_PR_curve_plots.png'), dpi=300)
 
-# Save validation metrics as a csv file
-results_df = pd.DataFrame(metric_results)
-savedir = 'Modeling/best_models/'
-results_df.to_csv(os.path.join(savedir, 'loreg_optimized_metric_scores.csv'), index=False)
+# main function
+def main():
+    train_results, test_results = [], []
+    random_state = 312
 
+    # Build pipeline
+    pipe = Pipeline([
+        ('scaler', StandardScaler()),
+        ('smote', SMOTE(random_state=random_state)),
+        ('logreg', LogisticRegression(max_iter=2000))
+    ])
+
+    # 5 kfold cv for hyperparameter otimization
+    kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=random_state)
+
+    # Model parameters
+    param_grid = {
+        'logreg__C': uniform(0.1, 10),
+        'logreg__solver': ['liblinear', 'lbfgs']
+    }
+
+    # Custom scorer for False Negative Rate
+    def fnr(y_test, y_pred):
+        tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+        fnr = fn / (fn + tp)
+        return fnr
+
+    # Scoring dictionary
+    scoring = {
+        'accuracy': 'accuracy',
+        'precision': make_scorer(precision_score, pos_label=1),
+        'recall': make_scorer(recall_score, pos_label=1),
+        'roc_auc': 'roc_auc',
+        'fnr': make_scorer(fnr),
+        'f1_weighted': make_scorer(f1_score, average='weighted', pos_label=1)
+    }
+
+    dir = 'Data/final_dataframes/'
+    output_dir = 'Modeling/best_models/results'
+    antibiotics = ['Gentamicin', 'Trimethoprim_Sulfamethoxazole', 'Ciprofloxacin',
+                    'Ampicillin', 'Cefazolin','Nitrofurantoin','Piperacillin_Tazobactam',
+                    'Levofloxacin', 'Ceftriaxone']
+  
+    # for loop over all antibiotics
+    for i in antibiotics:
+        print('\n')
+        print(f"==== Antibiotic: {i} ====")
+        train_data = pd.read_csv(f'{dir}{i}_train_data.csv').drop(['Year', 'anon_id'], axis=1)
+        test_data = pd.read_csv(f'{dir}{i}_test_data.csv').drop(['Year', 'anon_id'], axis=1)
+
+        X_train, y_train = train_data.drop(i, axis=1), (train_data[i]==2.0).astype(int)
+        X_test, y_test = test_data.drop(i, axis=1), (test_data[i]==2.0).astype(int)
+
+        trainer = ModelTrainer(param_grid, scoring, pipe, kf, random_state)
+        evaluator = ModelEvaluator(scoring, output_dir)
+        plotter = PlotGenerator(output_dir)
+
+        search = trainer.train(X_train, y_train)
+        best_model = search.best_estimator_
+        train_results.append(evaluator.evaluate_cv(search, i))
+
+        calibrated_model = trainer.calibrate(best_model, X_train, y_train)
+        test_results.append(evaluator.evaluate_test(best_model, calibrated_model, X_test, y_test, i))
+
+        y_pred_pre = best_model.predict(X_test)
+        y_proba_pre = best_model.predict_proba(X_test)[:, 1]
+        y_pred_post = calibrated_model.predict(X_test)
+        y_proba_post = calibrated_model.predict_proba(X_test)[:, 1]
+
+        plotter.plot_all(i, y_test, y_pred_post, y_proba_post, y_proba_pre)
+        plotter.plot_feature_importance(best_model, X_train, antibiotic=i)
+
+    pd.DataFrame(test_results).to_csv(os.path.join(output_dir, "logreg_metrics_test_results.csv"), index=False)
+    pd.DataFrame(train_results).to_csv(os.path.join(output_dir, "logreg_metrics_train_results.csv"), index=False)
+
+
+if __name__ == "__main__":
+    main()
